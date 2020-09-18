b9c87ab3add56fb2056e8fc0340021f71fc37b41
Sitoxil <70744759+Sitoxil@users.noreply.github.com>
2020-09-18 11:19:47 +0300

    Add files via upload

---
+++ Karimova_stack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Karimova_stack
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Stack stack = new Stack();
+      stack.Push("Первый");
+      stack.Push("Второй");
+      stack.Push("Третий");
+      stack.Push("Четвёртый");
+      while
+      (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("From Stack: {0}", obj);
+      }
+    }
+  }
+}
