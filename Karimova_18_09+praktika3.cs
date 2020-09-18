9aab5365c80e284a6f46d0217cf5e4b8500f7ba1
Sitoxil <70744759+Sitoxil@users.noreply.github.com>
2020-09-18 10:06:13 +0300

    Add files via upload

---
+++ Karimova_18_09+praktika3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Karimoa
+{
+  class Program_Karimova
+  {
+    static void Main(string[] args)
+    {
+      string[] text ={"Ангелина",
+        "Настя",
+        " +79263772622",
+        " 79263772622",
+        " (926)3772622" };
+        string pattern = @"[+]{0,1}[7]{0,1}[(]{0,1}\d{3}[)]{0,1}\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}";
+
+      foreach (string item in text)
+      {
+        if (Regex.IsMatch(item, pattern))
+        {
+          
+          Console.WriteLine(item + " это телефон");
+        }
+        else
+        {
+          Console.WriteLine(
+            string.Format("{0} это НЕ телефон", item)
+            );
+        }
+
+      }
+
+    }
+  }
+}
+
+
+
+
+
