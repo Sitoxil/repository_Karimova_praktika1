75568261600c414d84e5624a5daddd1217a86d37
Sitoxil <70744759+Sitoxil@users.noreply.github.com>
2020-09-18 11:36:32 +0300

    Add files via upload

---
+++ Karimova_slovar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Karimova_slovar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+      list["Estados Unidos"] = "United States of America";
+      list["Россия"] = "Россия";
+      list["Эспаня"] = "Испания";
+      Console.WriteLine(list["эспаня"]);
+      Console.WriteLine(list["РОССИЯ"]);
+      Console.Read();
+    }
+  }
+}
