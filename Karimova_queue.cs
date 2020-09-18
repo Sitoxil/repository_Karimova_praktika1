5b52ad12708b591d7a27e75fe569e9b42e05017c
Sitoxil <70744759+Sitoxil@users.noreply.github.com>
2020-09-18 11:15:11 +0300

    Add files via upload

---
+++ Karimova_queue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Karimova_queue
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();
+      queue.Enqueue("Ангелина");
+      queue.Enqueue("8");
+      queue.Enqueue("Егор");
+      queue.Enqueue("Физика");
+      while (queue.Count > 0)
+      {
+        object obj = queue.Dequeue();
+        Console.WriteLine("From Queue: {0}", obj);
+      }
+    }
+  }
+}
