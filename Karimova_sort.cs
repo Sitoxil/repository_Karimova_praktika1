9a810cb21b9f021356e7a2b7f4df8441e3803ab1
Sitoxil <70744759+Sitoxil@users.noreply.github.com>
2020-09-18 11:08:00 +0300

    Add files via upload
    1

---
+++ Karimova_sort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace AK
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList();
+      myList.Add("Ангелина");
+      myList.Add("Тимофей");
+      myList.Add("Настя");
+      myList.Add("Егор");
+
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      myList.Sort();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+    }
+  }
+}
+        
